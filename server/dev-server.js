1fae295ce438f94e133bce14f7678add9ab79ac6
wx <18729177374@163.com>
2019-04-09 23:45:10 +0800

    dev-server环境变量端口引用修改

--- server/dev-server.js
+++ server/dev-server.js
@@ -5,13 +5,15 @@ const WebpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('../webpack/webpack.dev.conf.js');
 const config = require('../config');
 const webpack = require('webpack');
+const HOST = process.env.HOST
+const PORT = process.env.PORT && Number(process.env.PORT);
 const options = {
   contentBase: './dist',
   inline: true, // 自动刷新(开启热模块替换时也要开启)
   hot: true, // 是否开启热模块替换（改为true 配合react-hot-loader）
   historyApiFallback: true, //在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
-  host: config.dev.host,
-  port: config.dev.port, //如果省略，默认8080
+  host: HOST || config.dev.host,
+  port: PORT || config.dev.port, //如果省略，默认8080
   publicPath: config.dev.assetsPublicPath,
   open: config.dev.autoOpenBrowser, // 自动开启浏览器
 };
